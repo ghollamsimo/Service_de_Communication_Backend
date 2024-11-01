d6288f0bb2e79776c397e385078d4160d86961da
hissoune <khalidhissoune962@gmail.com>
2024-11-01 11:49:18 +0100

    Add : add the implementation of the login interface

--- src/repository/implementations/auth.implementation.ts
+++ src/repository/implementations/auth.implementation.ts
@@ -2,18 +2,42 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../../schemas/user.schema';
 import { UserEntity } from '../../entities/user.entity';
-import {AuthInterface} from "../interfaces/auth.interface";
+import { AuthInterface } from "../interfaces/auth.interface";
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
-export class AuthImplementation implements AuthInterface{
+export class AuthImplementation implements AuthInterface {
     constructor(
-        @InjectModel(User.name) private readonly userModel: Model<UserDocument>
-    ) {}
+        @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+        private readonly jwtService: JwtService,
+    ) { }
 
     async register(userEntity: UserEntity): Promise<UserDocument> {
         const saltRounds = 10;
         userEntity.password = await bcrypt.hash(userEntity.password, saltRounds);
         const createdUser = new this.userModel(userEntity);
         return createdUser.save();
+    }
+
+
+    async login(userEntity: UserEntity): Promise<{  token: string }> {
+
+         
+        const user = await this.userModel.findOne({ email: userEntity.email });
+    if (!user) {
+
+        throw new Error('Invalid credentials');
+      }
+    const isPasswordValid = await bcrypt.compare(userEntity.password, user.password); 
+
+    if (!isPasswordValid) {
+
+        throw new Error('Invalid credentials');
+    }
+       
+    const token = this.jwtService.sign({ id: user._id, email: user.email });
+
+
+   return {token} ;
     }
 }
