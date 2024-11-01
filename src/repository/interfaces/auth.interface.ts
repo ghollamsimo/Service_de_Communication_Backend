e9a7483e2864a1d58f20755ac9bdd30c210c5023
hissoune <khalidhissoune962@gmail.com>
2024-11-01 11:48:30 +0100

    Add : add the function interface for the login

--- src/repository/interfaces/auth.interface.ts
+++ src/repository/interfaces/auth.interface.ts
@@ -3,4 +3,9 @@ import { UserEntity } from "../../entities/user.entity";
 
 export interface AuthInterface {
     register(user: UserEntity): Promise<UserDocument>;
+
+
+    login(user: UserEntity): Promise< {token: string} >;
+
+    
 }
