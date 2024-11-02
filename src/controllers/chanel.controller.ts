55b367290cffc47399176690cf5695941fddb3d4
hissoune <khalidhissoune962@gmail.com>
2024-11-02 13:42:47 +0100

    Add : adding the chenel controller

---
+++ src/controllers/chanel.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+
+
+@Controller('chanels')
+
+export class  ChanelsController {
+
+    constructor(parameters) {
+        
+    }
+
+    
+}
+
