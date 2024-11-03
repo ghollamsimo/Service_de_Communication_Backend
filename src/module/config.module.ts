6c7100c6b2cd6754e2ccdc224f312baa39267661
hissoune <khalidhissoune962@gmail.com>
2024-11-03 18:59:31 +0100

    Fix : fixing the config module and inject the chanel modul

--- src/module/config.module.ts
+++ src/module/config.module.ts
@@ -2,11 +2,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth.module';
+import { ChannelModule } from './chanel.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/communication'),
     AuthModule,
+    ChannelModule,
   ],
   controllers: [],
   providers: [],
