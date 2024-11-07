aeabdd2cf208500c3f437902f29099e65b122cd7
hissoune <khalidhissoune962@gmail.com>
2024-11-07 13:06:55 +0100

    Add : add the friend service and add the function of creating a friend request

---
+++ src/services/friend.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { FrienImplementatins } from '../repository/implementations/friend.implementation';
+import { FriendEntity } from '../entities/friend.entity';
+import { FriendDocument } from "src/schemas/friend.schema";
+
+
+
+
+@Injectable()
+
+export class FriendService{
+
+    constructor(private readonly FrienImplementatins:FrienImplementatins){}
+
+
+    async createFriendRequest(FriendEntity:FriendEntity):Promise<FriendDocument>{
+        return this.FrienImplementatins.createFriendRequest(FriendEntity);
+    }
+
+}
