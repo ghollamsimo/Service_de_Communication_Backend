373fb9310b455fe334a6d00be088a0ac672081e6
hissoune <khalidhissoune962@gmail.com>
2024-11-07 13:31:53 +0100

    Add : inject the friend modul in the confiig module

--- src/module/config.module.ts
+++ src/module/config.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth.module';
 import { ChannelModule } from './chanel.module';
+import { FriendModule } from './friend.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/communication'),
     AuthModule,
     ChannelModule,
+    FriendModule
   ],
   controllers: [],
   providers: [],
