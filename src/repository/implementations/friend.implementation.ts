439aa88151b6b74fcbb22554c007dcf55033c0eb
hissoune <khalidhissoune962@gmail.com>
2024-11-08 12:44:04 +0100

    Add : adding the friend accepting implementation and send a request to the auth service to add thoes friends and update the friend request to be accepted and return a msg

--- src/repository/implementations/friend.implementation.ts
+++ src/repository/implementations/friend.implementation.ts
@@ -3,6 +3,8 @@ import { Friend, FriendDocument } from "src/schemas/friend.schema";
 import { FriendInterface } from "../interfaces/friend.interface";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import axios from "axios";
+import { NotFoundException, UnauthorizedException } from "@nestjs/common";
 
 
 
@@ -17,5 +19,38 @@ export  class FrienImplementatins implements FriendInterface {
     const newfriend = new this.FriendMoodel(Friendentity)
        return newfriend.save() ;
    }
+
+   async acceptFriendRequest(accepterId: string, id: string): Promise<{ msg: string }> {
+    const request = await this.FriendMoodel.findById(id);
+
+    if (!request) {
+        throw new NotFoundException('Friend request not found');
+    }
+   
+    
+
+    if (request.receiverId !== accepterId) {
+        throw new UnauthorizedException('Unauthorized: Only the receiver can accept this request');
+    }
+
+    const body = {
+        accepterId: request.receiverId,
+        acceptedId: request.requesterId
+    };
+      try {
+            const response = await axios.patch('http://localhost:3002/auth/add_friend', body);
+    
+            const update = { status: 'accepted' };
+        
+            await this.FriendMoodel.findByIdAndUpdate(request._id, update, { new: true });
+    
+            return { msg: response.data.msg };
+
+      } catch (error) {
+        
+      }
+
+}
+
 
 }
