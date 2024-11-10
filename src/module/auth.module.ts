8c0e203d845d52df93b4debf6de7c6733b0d12eb
hissoune <khalidhissoune962@gmail.com>
2024-11-10 09:10:11 +0100

    Fix : fixing the user module and import the schema of the user and the model

--- src/module/auth.module.ts
+++ src/module/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from '../controllers/auth.controller';
 import { AuthService } from '../services/auth.service';
 import { AuthImplementation } from '../repository/implementations/auth.implementation';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserModelSchema } from 'src/schemas/user.schema';
 
 @Module({
   imports: [
    
-
+    MongooseModule.forFeature([{ name: User.name, schema: UserModelSchema }])
    
   ],
   controllers: [AuthController],
