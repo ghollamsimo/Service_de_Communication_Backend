353a02ef9d0d0c8c04d9167a24db678a4331e06f
hissoune <khalidhissoune962@gmail.com>
2024-11-10 11:37:29 +0100

    Fix : clearing the confiig module after the gard implimentations globaly fails

--- src/module/config.module.ts
+++ src/module/config.module.ts
@@ -8,9 +8,10 @@ import { FriendModule } from './friend.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/communication'),
+    
     AuthModule,
     ChannelModule,
-    FriendModule
+    FriendModule,
   ],
   controllers: [],
   providers: [],
