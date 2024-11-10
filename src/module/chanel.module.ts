8913b4ce8a797ed5e4c94a5a5cb25866e6b700b5
hissoune <khalidhissoune962@gmail.com>
2024-11-10 11:36:31 +0100

    Fix : adding the model of the user to the chanel module because the modlware is using the usermodel and we cant aply the modlware on the chanel routes while the dependencies doesnt know the usermodel

--- src/module/chanel.module.ts
+++ src/module/chanel.module.ts
@@ -4,14 +4,17 @@ import { ChannelSchema } from '../schemas/chanel.schema';
 import { ChanelsController } from 'src/controllers/chanel.controller';
 import { chanelService } from 'src/services/chanel.service';
 import { ChanelImplementations } from 'src/repository/implementations/chanel.implementation';
-import { AuthMidllware } from 'src/gards/auth.gard';
+import { User, UserModelSchema } from 'src/schemas/user.schema';
+
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Channel', schema: ChannelSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserModelSchema }])
+
   ],
   controllers: [ChanelsController],
-  providers: [chanelService,ChanelImplementations,AuthMidllware],
+  providers: [chanelService,ChanelImplementations],
   exports:[ChanelImplementations]
 })
 export class ChannelModule {}
