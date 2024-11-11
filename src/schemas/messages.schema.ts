bdc21c660f07cfc2fe3c5b1740db19f4aca18f79
hissoune <khalidhissoune962@gmail.com>
2024-11-11 15:17:46 +0100

    Add : adding the message schema

---
+++ src/schemas/messages.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+
+enum Status {
+    Sent = 'sent',
+    Readed = 'readed',
+    Failed = 'faild',
+  }
+
+@Schema({ timestamps: true })
+
+export class Message extends Document {
+
+  @Prop({ type: mongoose.Types.ObjectId ,ref:"User", required: true })
+  senderId: string; 
+  
+  @Prop({ type: mongoose.Types.ObjectId ,ref:"Channel", required: true })
+  channelId: string; 
+
+  @Prop({ type: String, required: true })
+  content: string; 
+
+  @Prop({ type: String, default: 'text' })
+  type: string; 
+
+  @Prop({ type: String, enum: Status, default: Status.Sent })
+  status: Status;
+}
+export const MessageSchema = SchemaFactory.createForClass(Message);
