212942d7d23a31b57e48c04cfb297b0e07905ad7
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-12 17:10:30 +0100

    Feat --> Adding message.controller.ts in folder controller

---
+++ src/controllers/message.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { MessageService } from "src/services/message.service";
+
+@Controller('message')
+class MessageController{
+    constructor(private readonly MessageService: MessageService) { }
+    
+    @Post()
+    async create(@Body() body: { senderId: string, receiverId: string, content: string, type: 'sent', }) {
+        
+    }
+}
