fcae668bc47d913f737e19e8e9dee6585d7e2ae0
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-12 15:59:41 +0100

    Feat --> Adding notification.module file in folder module and when you create a friend send anotification to the receiver and when you accepte a friend send a notification to sender

--- src/controllers/friend.controller.ts
+++ src/controllers/friend.controller.ts
@@ -1,75 +1,61 @@
-import { BadRequestException, Body, Controller, Param, Patch, Post, Request, UseGuards } from "@nestjs/common";
-import { FriendService } from '../services/friend.service';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Param,
+    Patch,
+    Post,
+    Request,
+    UseGuards,
+} from "@nestjs/common";
+import { FriendService } from "../services/friend.service";
 import { AuthMidllware } from "src/gards/auth.gard";
-import {  FriendEntity } from '../entities/friend.entity';
+import { FriendEntity } from "../entities/friend.entity";
 import { Types } from "mongoose";
 
-
-
-
-@Controller('friends')
+@Controller("friends")
 @UseGuards(AuthMidllware)
+export class FriendController {
+    constructor(private readonly FriendService: FriendService) { }
 
-export class FriendController{
-
-    constructor(private readonly FriendService:FriendService){}
-
-
-
-
-    @Post('send')
-
+    @Post("send")
     createFriendRequest(
         @Request() req,
-        @Body() body:{
-            receiverId:string,   
-        }
-    ){
-        const  requesterId = req.user._id;
+        @Body()
+        body: {
+            receiverId: string;
+        },
+    ) {
+        const requesterId = req.user._id;
         if (!Types.ObjectId.isValid(body.receiverId)) {
-            throw new BadRequestException('Invalid reciver id');
+            throw new BadRequestException("Invalid reciver id");
         }
-        const Friendentity = new  FriendEntity(requesterId, body.receiverId);
-        
+        const Friendentity = new FriendEntity(requesterId, body.receiverId);
+
         return this.FriendService.createFriendRequest(Friendentity);
-
-
-
     }
-    @Patch('accept/:id')
-    accepteFriend(
-        @Param('id') id:string,
-        @Request() req,
-    ){
+    @Patch("accept/:id")
+    accepteFriend(@Param("id") id: string, @Request() req) {
         const accepterId = req.user._id;
 
         return this.FriendService.acceptFriendRequest(accepterId, id);
     }
 
-    @Patch('block/:id')
-    blockFriend(
-        @Param('id') id:string,
-        @Request() req,
-    ){
-       const blockerId = req.user._id;
-       return this.FriendService.blockFriend(blockerId, id)
+    @Patch("block/:id")
+    blockFriend(@Param("id") id: string, @Request() req) {
+        const blockerId = req.user._id;
+        return this.FriendService.blockFriend(blockerId, id);
     }
 
-    @Patch('cancel/:id')
-    cancelFriend(
-        @Param('id') id:string,
-        @Request() req,
-    ){
-       const cancelerId = req.user._id;
-       return this.FriendService.cancelFriend(cancelerId, id)
+    @Patch("cancel/:id")
+    cancelFriend(@Param("id") id: string, @Request() req) {
+        const cancelerId = req.user._id;
+        return this.FriendService.cancelFriend(cancelerId, id);
     }
 
-    @Patch('unblock/:id')
-    unblockFriend(
-        @Param('id') id:string,
-        @Request() req,
-    ){
-       const unblockerId = req.user._id;
-       return this.FriendService.unblockFriend(unblockerId, id)
+    @Patch("unblock/:id")
+    unblockFriend(@Param("id") id: string, @Request() req) {
+        const unblockerId = req.user._id;
+        return this.FriendService.unblockFriend(unblockerId, id);
     }
 }
--- src/repository/implementations/friend.implementation.ts
+++ src/repository/implementations/friend.implementation.ts
@@ -6,139 +6,140 @@ import { Model } from "mongoose";
 import { BadRequestException, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { User, UserDocument } from "src/schemas/user.schema";
 import { Channel, ChannelDocument } from '../../schemas/chanel.schema';
+import { Notification, NotificationDocument } from '../../schemas/notification.schema';
 
 
 
 export  class FrienImplementatins implements FriendInterface {
- 
     constructor(
-        @InjectModel(Friend.name) private readonly FriendMoodel:Model<FriendDocument>,
-        @InjectModel(User.name) private readonly UserModel: Model<UserDocument> ,
-        @InjectModel(Channel.name) private readonly ChannelModel: Model<ChannelDocument> ,
-
-
-){}
-
-
-
-   createFriendRequest(Friendentity: FriendEntity): Promise<FriendDocument> {
-     
-     if (Friendentity.receiverId === Friendentity.requesterId) {
-         throw new UnauthorizedException('you cant send a friend request to your self');
-     }
-    const newfriend = new this.FriendMoodel(Friendentity)
-       return newfriend.save() ;
-   }
-
-
-
-   async acceptFriendRequest(accepterId: string, id: string): Promise<{ msg: string}> {
-    const request = await this.FriendMoodel.findById(id);
-  
-    if (!request) {
-      throw new NotFoundException('Friend request not found');
-    }
-    
-    if (request.receiverId.toString() !== accepterId.toString()) {
-      throw new UnauthorizedException('Unauthorized: Only the receiver can accept this request');
-    }
-  
-    await this.FriendMoodel.findByIdAndUpdate(request._id, { status: 'accepted' }, { new: true });
-  
-    await this.UserModel.updateOne(
-      { _id: request.receiverId },
-      { $addToSet: { friends: request.requesterId } }
-    );
-    await this.UserModel.updateOne(
-      { _id: request.requesterId },
-      { $addToSet: { friends: request.receiverId } }
-    );
-  
-    const newChannel = await this.ChannelModel.create({
-        name:"direct message",
-      members: [
-        { userId: request.requesterId, role: 'member' },
-        { userId: request.receiverId, role: 'member' }
-      ],
-      type: 'dm'
-    });
-  
-    return {  msg: 'Friend request accepted successfully, and a new DM channel created' };
-  }
-
-
-async blockFriendRequest(blockerId: string, id: string): Promise<{ msg: string }> {
-    const request = await this.FriendMoodel.findById(id);
-
-    if (!request) {
-        throw new NotFoundException('Friend request not found');
+        @InjectModel(Friend.name) private readonly FriendModel: Model<FriendDocument>,
+        @InjectModel(User.name) private readonly UserModel: Model<UserDocument>,
+        @InjectModel(Notification.name) private readonly NotificationModel: Model<NotificationDocument>,
+    ) { }
+    UnblockFriendRequest(unblockerId: string, id: string): Promise<{ msg: string; }> {
+        throw new Error("Method not implemented.");
     }
 
-    if (request.status !== 'accepted') {
-        throw new UnauthorizedException('Only an accepted friend request can be blocked');
+    async createFriendRequest(FriendEntity: FriendEntity): Promise<FriendDocument> {
+        if (FriendEntity.receiverId === FriendEntity.requesterId) {
+            throw new UnauthorizedException("You can't send a friend request to yourself");
+        }
+
+        const newFriend = new this.FriendModel(FriendEntity);
+
+        await this.NotificationModel.create({
+            type: 'friend_request',
+            content: `${FriendEntity.requesterId} sent a friend request.`,
+            senderId: FriendEntity.requesterId,
+            receiverId: FriendEntity.receiverId,
+        });
+
+        return newFriend.save();
     }
 
-    await this.FriendMoodel.findByIdAndUpdate(request._id, { status: 'blocked' }, { new: true });
+    async acceptFriendRequest(accepterId: string, id: string): Promise<{ msg: string }> {
+        const request = await this.FriendModel.findById(id);
 
-    await this.UserModel.updateOne(
-        { _id: request.receiverId },
-        { $pull: { friends: request.requesterId } }
-    );
-    await this.UserModel.updateOne(
-        { _id: request.requesterId },
-        { $pull: { friends: request.receiverId } }
-    );
+        if (!request) {
+            throw new NotFoundException('Friend request not found');
+        }
 
-    return { msg: 'Friend request blocked successfully' };
-}
+        if (request.receiverId !== accepterId.toString()) {
+            throw new UnauthorizedException('Unauthorized: Only the receiver can accept this request');
+        }
 
+        await this.FriendModel.findByIdAndUpdate(
+            request._id,
+            { status: 'accepted' },
+            { new: true }
+        );
 
-async cancelFriendRequest(cancelerId: string, id: string): Promise<{ msg: string }> {
-    const request = await this.FriendMoodel.findById(id);
+        await this.UserModel.updateOne(
+            { _id: request.receiverId },
+            { $addToSet: { friends: request.requesterId } }
+        );
+        await this.UserModel.updateOne(
+            { _id: request.requesterId },
+            { $addToSet: { friends: request.receiverId } }
+        );
 
-    if (!request) {
-        throw new NotFoundException('Friend request not found');
-    }
-   
-    
-    if (request.requesterId !== cancelerId.toString()) {
-        throw new UnauthorizedException('Unauthorized: Only the requester can cancel this request');
-    }
+        await this.NotificationModel.create({
+            type: 'friend_request',
+            content: `${request.receiverId} has accepted your friend request.`,
+            senderId: request.receiverId,
+            receiverId: request.requesterId,
+        });
 
-    if (request.status === 'Accepted') {
-        throw new BadRequestException('Cannot cancel an accepted friend request. You can only block the friend');
-    }
-
-    await this.FriendMoodel.findByIdAndDelete(id);
-
-    return { msg: 'Friend request canceled successfully' };
-}
-
-async UnblockFriendRequest(unblockerId: string, id: string): Promise<{ msg: string }> {
-    const request = await this.FriendMoodel.findById(id);
-
-    if (!request) {
-        throw new NotFoundException('Friend request not found');
+        return { msg: 'Friend request accepted successfully' };
     }
 
-    if (request.status !== 'blocked') {
-        throw new UnauthorizedException('Only a blocked friend request can be unblocked');
+    async blockFriendRequest(blockerId: string, id: string): Promise<{ msg: string }> {
+        const request = await this.FriendModel.findById(id);
+
+        if (!request) {
+            throw new NotFoundException('Friend request not found');
+        }
+
+        if (request.status !== 'accepted') {
+            throw new UnauthorizedException('Only an accepted friend request can be blocked');
+        }
+
+        await this.FriendModel.findByIdAndUpdate(request._id, { status: 'blocked' }, { new: true });
+
+        await this.UserModel.updateOne(
+            { _id: request.receiverId },
+            { $pull: { friends: request.requesterId } }
+        );
+        await this.UserModel.updateOne(
+            { _id: request.requesterId },
+            { $pull: { friends: request.receiverId } }
+        );
+
+        return { msg: 'Friend request blocked successfully' };
     }
 
-    await this.FriendMoodel.findByIdAndUpdate(request._id, { status: 'accepted' }, { new: true });
+    async cancelFriendRequest(cancelerId: string, id: string): Promise<{ msg: string }> {
+        const request = await this.FriendModel.findById(id);
 
-    await this.UserModel.updateOne(
-        { _id: request.receiverId },
-        { $addToSet: { friends: request.requesterId } }
-    );
-    await this.UserModel.updateOne(
-        { _id: request.requesterId },
-        { $addToSet: { friends: request.receiverId } }
-    );
+        if (!request) {
+            throw new NotFoundException('Friend request not found');
+        }
 
-    return { msg: 'Friend unblocked successfully' };
-}
+        if (request.requesterId !== cancelerId.toString()) {
+            throw new UnauthorizedException('Unauthorized: Only the requester can cancel this request');
+        }
 
+        if (request.status === 'accepted') {
+            throw new BadRequestException('Cannot cancel an accepted friend request. You can only block the friend');
+        }
 
+        await this.FriendModel.findByIdAndDelete(id);
 
+        return { msg: 'Friend request canceled successfully' };
+    }
+
+    async unblockFriendRequest(unblockerId: string, id: string): Promise<{ msg: string }> {
+        const request = await this.FriendModel.findById(id);
+
+        if (!request) {
+            throw new NotFoundException('Friend request not found');
+        }
+
+        if (request.status !== 'blocked') {
+            throw new UnauthorizedException('Only a blocked friend request can be unblocked');
+        }
+
+        await this.FriendModel.findByIdAndUpdate(request._id, { status: 'accepted' }, { new: true });
+
+        await this.UserModel.updateOne(
+            { _id: request.receiverId },
+            { $addToSet: { friends: request.requesterId } }
+        );
+        await this.UserModel.updateOne(
+            { _id: request.requesterId },
+            { $addToSet: { friends: request.receiverId } }
+        );
+
+        return { msg: 'Friend unblocked successfully' };
+    }
 }
