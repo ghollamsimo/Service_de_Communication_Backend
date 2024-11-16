795a9c7d36b98d7c8deca9c5ff4b4dbf7419ea3a
hissoune <khalidhissoune962@gmail.com>
2024-11-16 19:40:55 +0100

    Add : adding the interface function of getting the user friends

--- src/repository/interfaces/friend.interface.ts
+++ src/repository/interfaces/friend.interface.ts
@@ -6,6 +6,8 @@ import { FriendEntity } from '../../entities/friend.entity';
 export interface FriendInterface{
 
   createFriendRequest(FriendEntity:FriendEntity):Promise<FriendDocument>;
+  getmyfriends(id:string):Promise<FriendDocument[]>;
+
   acceptFriendRequest(accepterId:string,id:string):Promise<{msg:string}>;
   blockFriendRequest(blockerId:string,id:string):Promise<{msg:string}>;
   UnblockFriendRequest(unblockerId:string,id:string):Promise<{msg:string}>;
