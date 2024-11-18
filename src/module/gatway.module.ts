29602ab0202fc2ec5a6fcd42e3f26169b68b48cc
hissoune <khalidhissoune962@gmail.com>
2024-11-18 18:28:55 +0100

    Add : injecting the message model in the gatway module to handel the createing message befor emiting

--- src/module/gatway.module.ts
+++ src/module/gatway.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { WebRtcGateway } from "src/gateways/webrtc.gateway";
 import { ChannelSchema } from "src/schemas/chanel.schema";
+import { Message, MessageSchema } from "src/schemas/messages.schema";
 import { User, UserModelSchema } from "src/schemas/user.schema";
 
 
@@ -12,6 +13,8 @@ import { User, UserModelSchema } from "src/schemas/user.schema";
     imports:[
         MongooseModule.forFeature([{ name: User.name, schema: UserModelSchema }]),
         MongooseModule.forFeature([{ name: 'Channel', schema: ChannelSchema }]),
+        MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+
 
     ],
     providers:[WebRtcGateway]
